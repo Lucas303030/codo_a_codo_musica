ea84419b65cd4a2a428d073b5be87b84aa5475c8
Lucas <115429096+Lucas303030@users.noreply.github.com>
2023-04-26 03:36:38 -0300

    Preloader on load

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// Preloader (Cambiar por loader animado - Prueba)
+
+const preloader = document.querySelector(".preloader")
+
+window.addEventListener("load", function (e) {
+    alert("Carga completa")
+    preloader.classList.add("oculto") 
+ })
+
 // Intersection Observer
 
 const sections = document.querySelectorAll("section.estilo")
@@ -7,8 +16,7 @@ function intersec(entradas){
         if (entrada.isIntersecting){
             entrada.target.classList.add("active")
         }
-        }
-    )
+        })
 }
 
 const observer = new IntersectionObserver(intersec)
